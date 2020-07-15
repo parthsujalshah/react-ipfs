c96b89c806b03e413e0d873c7a4b34e33877fc2c
parthsujalshah <parthsujalshah@gmail.com>
2020-07-15 17:09:22 +0530

    file selection

---
+++ src/components/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import logo from '../logo.png';
+import './App.css';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      buffer: null
+    };
+  }
+
+  captureFile = event => {
+      event.preventDefault();
+    const file = event.target.files[0];
+    const reader = new window.FileReader(); // converts file to buffer
+    reader.readAsArrayBuffer(file);
+    reader.onloadend = () => {
+      this.setState({ buffer: Buffer(reader.result) });
+    };
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+    console.log('here');
+  };
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+          <a
+            className="navbar-brand col-sm-3 col-md-2 mr-0"
+            href="http://www.dappuniversity.com/bootcamp"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Meme of the Day
+          </a>
+        </nav>
+        <div className="container-fluid mt-5">
+          <div className="row">
+            <main role="main" className="col-lg-12 d-flex text-center">
+              <div className="content mr-auto ml-auto">
+                <a
+                  href="http://www.dappuniversity.com/bootcamp"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={logo} className="App-logo" alt="logo" />
+                </a>
+                <p>&nbsp;</p>
+                <h2>Change Meme</h2>
+                <form>
+                  <input type="file" onChange={this.captureFile} />
+                  <input type="submit" onSubmit={this.onSubmit} />
+                </form>
+              </div>
+            </main>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
